90f82b51590d3d0666d6b673d7de156c161dcbee
conscious <conscious>
2016-12-20 17:07:07 +0900

    Added SearchResult

---
+++ app/src/main/java/com/tkkim/whereisit/search/SearchResult.java
@@ -0,0 +1,31 @@
+package com.tkkim.whereisit.search;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchResult extends Fragment {
+
+    public SearchResult() {
+    }
+
+    public static SearchResult newInstance(String param1, String param2) {
+        SearchResult fragment = new SearchResult();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
